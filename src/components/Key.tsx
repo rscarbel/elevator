9299d7832f9e0af713912edd1d353ba8be8c8982
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-05 10:40:09 -0500

    added floorQueue as parameter & ...
    added "queued-key" class to highlight key when it is in the queue

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -3,11 +3,15 @@ import React from "react"
 interface KeyProps {
   floor: number;
   yAxisPos: number;
+  floorQueue: number[];
   addFloorToQueue: Function;
 }
 
-const Key: React.FC<KeyProps> = ({ floor, addFloorToQueue, yAxisPos }) => {
-  return <div className='key' onClick={ function(){addFloorToQueue(floor)} }>
+const Key: React.FC<KeyProps> = ({ floor, addFloorToQueue, floorQueue,  yAxisPos }) => {
+
+  const isInQueue: boolean = floorQueue.indexOf(floor) === -1 ? false : true;
+
+  return <div className={`key ${isInQueue ? 'queued-key' : ''}`} onClick={ function(){addFloorToQueue(floor)} }>
     <p>{ floor }</p>
   </div>;
 };
