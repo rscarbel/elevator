9db21d29bc88b98d7d4393e83ecb70b9ca7d063b
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-05 10:20:22 -0500

    Key now sends yAxis position instead of floor num

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -2,11 +2,12 @@ import React from "react"
 
 interface KeyProps {
   floor: number;
+  yAxisPos: number;
   changeFloor: Function;
 }
 
-const Key: React.FC<KeyProps> = ({ floor, changeFloor }) => {
-  return <div className='key' onClick={ function(){changeFloor(floor)} }>
+const Key: React.FC<KeyProps> = ({ floor, changeFloor, yAxisPos }) => {
+  return <div className='key' onClick={ function(){changeFloor(yAxisPos)} }>
     <p>{ floor }</p>
   </div>;
 };
