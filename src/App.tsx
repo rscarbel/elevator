a7fb321c4d64a70b73d3366ff8099dbd0b76a3fb
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-05 08:22:41 -0500

    Added todos for the day
    I didn't write them as tests, because I'm pressed for time & this isn't
    really going into production.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/*
+TODO
+
+make readme
+
+create building with 10 floors
+    display floor number on each floor
+create elevator
+create keypad with 10 keys
+    keys need to light up when pressed
+    keys need to remain lighted up when elevator has not reached them
+
+make elevator move
+    it needs to go to the floor of the button that was pressed
+    it needs to have a priority queue
+    it needs to stop at floors inbetween the current floor and the target floor
+    it needs to wait 3 seconds at stopped floors
+display current floor
+    it should update as the floor moves
+*/
 
 function App() {
   return (
