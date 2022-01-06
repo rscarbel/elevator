72f66e3a7dfccda833d8b1803412814dde36a53d
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-06 17:52:28 -0500

    Added conditions for changing styles

--- src/components/ChangeHeightInterface.tsx
+++ src/components/ChangeHeightInterface.tsx
@@ -1,20 +1,47 @@
+import { floor } from "cypress/types/lodash";
 import React from "react";
 
 interface ChangeHeightProps {
   incrementHeight: Function;
   decrementHeight: Function;
   height: number;
+  isActive: boolean;
 }
 
-const ChangeHeightInterface: React.FC<ChangeHeightProps> = ({ incrementHeight, decrementHeight, height}) => {
+const deactivatedStyle = {
+  color: '#959494',
+  backgroundColor: '#d5d5d5',
+  pointerEvents: 'none',
+}
+
+const incrementStyle = {
+  color: '#FFFFFF',
+  backgroundColor: '#009910'
+}
+
+const decrementStyle = {
+  color: '#000000',
+  backgroundColor: '#ffd13d'
+}
+
+const ChangeHeightInterface: React.FC<ChangeHeightProps> = ({ incrementHeight, decrementHeight, height, isActive}) => {
   return <div className="change-height">
+
     <p>Change floor height:</p>
-    <button onClick={function(){incrementHeight()}}>+</button>
-    <button onClick={function(){
+
+    <button
+    style={isActive ? incrementStyle : deactivatedStyle}
+    onClick={function(){incrementHeight()
+    }}>+</button>
+
+    <button
+    style={isActive && !(height <= 5) ? decrementStyle : deactivatedStyle}
+    onClick={function(){
       if (height > 5) {
         decrementHeight()
       }
       }}>-</button>
+
   </div>
 };
 
