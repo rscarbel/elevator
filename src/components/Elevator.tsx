65e32888059f7d377b5012bb7d3676cf8e535309
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-06 10:43:47 -0500

    added parameter for height
    I subtracted 2 from the height to account for the border size

--- src/components/Elevator.tsx
+++ src/components/Elevator.tsx
@@ -3,11 +3,13 @@ import React from "react";
 interface ElevatorProps {
   currentFloor: number;
   currentYPosition: number;
+  height: number;
 }
 
 
-const Elevator: React.FC<ElevatorProps> = ({ currentFloor, currentYPosition }) => {
-  return <div className='elevator' style={ { bottom: `${ currentYPosition }px`} }>
+const Elevator: React.FC<ElevatorProps> = ({ currentFloor, currentYPosition, height }) => {
+  return <div className='elevator' style={ { bottom: `${ currentYPosition }px`,
+  height: `${ height - 2 }px`} }>
     <p>{ currentFloor }</p>
   </div>;
 };
