25c6e3867af7ea4a90d47b19cd0eeb518142e888
Ryan Scarbel <ryanscarbel@gmail.com>
2022-01-06 19:39:20 -0500

    removed floor reference
    the value is being used from the css var

--- src/components/Floor.tsx
+++ src/components/Floor.tsx
@@ -4,13 +4,12 @@ import styleSizes from "../scripts/styleSizes";
 
 interface FloorProps {
   floor: number;
-  height: number;
 }
 
-const Floor: React.FC<FloorProps> = ({ floor, height }) => {
+const Floor: React.FC<FloorProps> = ({ floor }) => {
 
   return <div className='floor' style={
-    {height: `${height - styleSizes.borderWidthValue}px`,
+    {
       borderBottomWidth: floor === 1 ? styleSizes.borderWidthValue : 0}
     }>
     <p>{ floor }</p>
